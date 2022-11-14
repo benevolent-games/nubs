ffde6ea8496aa4147535973f8b66d3f740520564
Chase Moskal <chasemoskal@gmail.com>
2022-11-14 14:23:59 -0800

    update: magical, refactor tracking

--- s/components/stick/component.ts
+++ s/components/stick/component.ts
@@ -7,11 +7,7 @@ import * as v2 from "../../tools/v2.js"
 import {attachEvents} from "../../tools/attach-events.js"
 import {asLitListener} from "../../tools/lit-listener.js"
 import {findTouchAppleFriendly} from "../../tools/find-touch-ios-friendly.js"
-
-export interface Basis {
-	rect: DOMRect
-	radius: number
-}
+import {setupStickTracking} from "./utils/tracking.js"
 
 export const NubStick = component({
 		styles,
@@ -30,69 +26,11 @@ export const NubStick = component({
 		understick: "",
 	})
 
-	function getBasis(): undefined | Basis {
-		const base = use.element.shadowRoot?.querySelector<HTMLElement>(".base")
-		const stick = use.element.shadowRoot?.querySelector<HTMLElement>(".stick")
-		if (base && stick) {
-			const rect = base?.getBoundingClientRect()!
-			const radius = (rect.width / 2) - (stick.getBoundingClientRect().width / 4)
-			return {rect, radius}
-		}
-		else
-			return undefined
-	}
-
-	const {resetStick, moveStick} = (() => {
-		const withinRadius = (basis: Basis, x: number, y: number) => {
-			return (x ** 2) + (y ** 2) < (basis.radius ** 2)
-		}
-	
-		const findClosestPointOnCircle = (basis: Basis, x: number, y: number) => {
-			const mag = Math.sqrt((x ** 2) + (y ** 2))
-			return [
-				x / mag * basis.radius,
-				y / mag * basis.radius,
-			]
-		}
-	
-		const registerFinalValues = (basis: Basis, x: number, y: number) => {
-			const values: v2.V2 = [
-				x / basis.radius,
-				-(y / basis.radius),
-			]
-			const underX = x * 0.5
-			const underY = y * 0.5
-			setStyleTransforms({
-				stick: `transform: translate(${x}px, ${y}px);`,
-				understick: `transform: translate(${underX}px, ${underY}px);`,
-			})
-			triggerInput(values)
-		}
-	
-		const moveStick = (clientX: number, clientY: number) => {
-			const basis = getBasis()
-			if (basis) {
-				console.log("move stick")
-				const {left, top, height, width} = basis.rect
-				const middleX = left + (width / 2)
-				const middleY = top + (height / 2)
-				let x = clientX - middleX
-				let y = clientY - middleY
-				if (!withinRadius(basis, x, y))
-					[x, y] = findClosestPointOnCircle(basis, x, y)
-				registerFinalValues(basis, x, y)
-			}
-		}
-	
-		const resetStick = () => {
-			const basis = getBasis()
-			if (basis)
-				registerFinalValues(basis, 0, 0)
-		}
-
-		return {moveStick, resetStick}
-	})()
-
+	const {moveStick, resetStick} = setupStickTracking({
+		shadow: use.element.shadowRoot!,
+		setStyleTransforms,
+		triggerInput,
+	})
 
 	use.setup(() => attachEvents(window, {
 		mouseup() {
