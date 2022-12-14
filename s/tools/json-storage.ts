b2e5a3e47259b4a8798a284ba3c1bcef62d3f89e
Chase Moskal <chasemoskal@gmail.com>
2022-12-13 17:12:59 -0800

    refactor: and improve json storage

--- s/tools/json-storage.ts
+++ s/tools/json-storage.ts
@@ -2,22 +2,12 @@
 export function jsonStorage(storage: Storage) {
 	return {
 
-		getItem<T>(key: string) {
-			const text = storage.getItem(key)
-			try {
-				return text
-					? <T>JSON.parse(text)
-					: undefined
-			}
-			catch (error) {
-				console.warn("json storage proxy, 'get' error", error)
-			}
+		getItem<T>(key: string): T | undefined {
+			return jsonGet(storage, key)
 		},
 
-		setItem<T>(key: string, data: T) {
-			const text = JSON.stringify(data)
-			storage.setItem(key, text)
-			return true
+		setItem<T>(key: string, data: T | undefined) {
+			return jsonSet(storage, key, data)
 		},
 	}
 }
@@ -25,23 +15,35 @@ export function jsonStorage(storage: Storage) {
 export function jsonStorageProxy<T extends {[key: string]: any}>(
 		storage: Storage
 	) {
+	return <{[P in keyof T]: T[P] | undefined}>new Proxy({}, {
 
-	return <T>new Proxy({}, {
 		get(t, key: string) {
-			const text = storage.getItem(key)
-			try {
-				return text
-					? JSON.parse(text)
-					: undefined
-			}
-			catch (error) {
-				console.warn("json storage proxy, 'get' error", error)
-			}
+			return jsonGet(storage, key)
 		},
+
 		set(t, key: string, data) {
-			const text = JSON.stringify(data)
-			storage.setItem(key, text)
-			return true
+			return jsonSet(storage, key, data)
 		},
 	})
+}
+
+function jsonGet<T>(storage: Storage, key: string): T | undefined {
+	const text = storage.getItem(key)
+	try {
+		return text
+			? JSON.parse(text)
+			: undefined
+	}
+	catch (error) {
+		console.warn("json storage 'get' error", error)
+	}
+}
+
+function jsonSet<T>(storage: Storage, key: string, data: T | undefined) {
+	const dataIsUnset = data === undefined || data === null
+	const text = dataIsUnset
+		? ""
+		: JSON.stringify(data)
+	storage.setItem(key, text)
+	return true
 }
