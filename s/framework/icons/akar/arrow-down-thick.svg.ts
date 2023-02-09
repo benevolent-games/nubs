4cf18d19248bb0963fdf3d1717f48cb75faf5c3e
Chase Moskal <chasemoskal@gmail.com>
2023-02-09 10:53:04 -0800

    add: more icons

---
+++ s/framework/icons/akar/arrow-down-thick.svg.ts
@@ -0,0 +1,6 @@
+
+import {svg} from "lit"
+
+export default svg`
+<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ai ai-ArrowDownThick"><path d="M12 21l9-7h-4.99L16 3H8v11H3l9 7z"/></svg>
+`
