0ee4ae0dd066ac0f760969b1ae447c9bfcc90c5f
zenkyu <przemekgg2002@gmail.com>
2023-02-19 13:13:37 +0100

    refactor: nub stickpad

--- s/elements/stickpad/element.ts
+++ s/elements/stickpad/element.ts
@@ -6,9 +6,21 @@ import {attachEvents} from "../../tools/attach-events.js"
 
 @mixinCss(styles)
 export class NubStickpad extends MagicElement {
+
+	get nubStickBasePart() {
+		return this.shadowRoot?.querySelector("nub-stick")!
+			.shadowRoot?.querySelector("[part='base']")!
+	}
+
 	realize(use: UseElement<this>): void | TemplateResult<1 | 2> {
 		const [stick, setStick] = use.state(false)
 		const [position, setPosition] = use.state("")
+
+		const setCenterPosition = (e : PointerEvent) => {
+			setStick(true)
+			const {clientWidth, clientHeight} = this.nubStickBasePart
+			setPosition(`left: ${e.pageX - clientWidth / 2}px; top: ${e.pageY - clientHeight / 2}px;`)
+		}
 
 		use.setup(() => {
 			attachEvents(window, {
@@ -19,13 +31,7 @@ export class NubStickpad extends MagicElement {
 		return html`
 			<div
 				class="area"
-				@pointerdown=${(e: PointerEvent) => {
-					const {pageX, pageY} = <PointerEvent>e
-					setStick(true)
-					const nubStick = this.shadowRoot?.querySelector("nub-stick")
-					const {clientWidth, clientHeight} = nubStick?.shadowRoot?.querySelector("[part='base']")!
-					setPosition(`left: ${pageX - clientWidth / 2}px; top: ${pageY - clientHeight / 2}px;`)
-				}}
+				@pointerdown=${(e: PointerEvent) => setCenterPosition(e)}
 			>
 				<nub-stick transform=${position} ?data-visible=${stick}></nub-stick>
 			</div>
