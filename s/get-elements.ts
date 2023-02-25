81811b3852c6d631377a94093719611cb2d6a9c0
Chase Moskal <chasemoskal@gmail.com>
2023-02-25 11:46:53 -0800

    deactivate: editor

--- s/get-elements.ts
+++ s/get-elements.ts
@@ -1,7 +1,7 @@
 
 import {NubStick} from "./elements/stick/element.js"
 import {NubRealMouse} from "./elements/real/mouse.js"
-import {NubEditor} from "./elements/editor/element.js"
+// import {NubEditor} from "./elements/_editor/element.js"
 import {NubContext} from "./elements/context/element.js"
 import {NubGridMenu} from "./elements/grid/menu/element.js"
 import {NubRealKeyboard} from "./elements/real/keyboard.js"
@@ -10,7 +10,7 @@ import {NubVisualizer} from "./elements/visualizer/element.js"
 
 export const getElements = () => ({
 	NubStick,
-	NubEditor,
+	// NubEditor,
 	NubContext,
 	NubGridMenu,
 	NubGridboard,
