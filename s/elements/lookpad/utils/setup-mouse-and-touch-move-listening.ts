fac479aa5fd5cefc99d8f1ebc5b6b1d801d090eb
Paul_Aroo <timmypaul2006@gmail.com>
2023-02-26 15:04:39 +0100

    fix: move only on click and drag

--- s/elements/lookpad/utils/setup-mouse-and-touch-move-listening.ts
+++ s/elements/lookpad/utils/setup-mouse-and-touch-move-listening.ts
@@ -8,17 +8,27 @@ export function setupMouseAndTouchMoveListening(
 		element: LitElement & {name: string}
 	) {
 
+	let isPointerDown = false
+
 	return attachEvents(element, {
+		pointerdown() {
+			isPointerDown = true
+		},
 		pointermove(e) {
-			const {movementX, movementY, clientX, clientY} = e as PointerEvent
-			NubInputEvent
-				.target(element)
-				.dispatch({
-					name: element.name,
-					type: Nub.Type.Mouse,
-					position: [clientX, clientY],
-					movement: [movementX, movementY],
-				})
+			if(isPointerDown) {
+				const {movementX, movementY, clientX, clientY} = e as PointerEvent
+				NubInputEvent
+					.target(element)
+					.dispatch({
+						name: element.name,
+						type: Nub.Type.Mouse,
+						position: [clientX, clientY],
+						movement: [movementX, movementY],
+					})
+			}
+		},
+		pointerup() {
+			isPointerDown = false
 		}
 	})
 }
