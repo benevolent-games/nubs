4c4ecdabfdaf955cda8a48986857b021725c4ed5
Paul_Aroo <timmypaul2006@gmail.com>
2023-07-16 21:05:23 +0100

    add: tests for bindings versioning

--- s/tests.test.ts
+++ s/tests.test.ts
@@ -1,6 +1,10 @@
 
 import {Suite, expect} from "cynic"
+
+import {converter_1} from "./elements/context/bindings/converters/1.js"
+import {Bindings1, Bindings2} from "./elements/context/bindings/types/bindings.js"
 import {migrate_versions} from "./elements/context/bindings/utils/migrate_versions.js"
+import {detect_bindings_version} from "./elements/context/bindings/utils/detect_bindings_version.js"
 
 export default <Suite>{
 	"version migrator": {
@@ -39,6 +43,90 @@ export default <Suite>{
 			expect(called.get(2)).ok()
 			expect(called.get(3)).ok()
 			expect(called.get(4)).ok()
+		},
+
+	},
+	"detecting bindings versions": {
+
+		"can detect version of bindings1": async () => {
+			const test_bindings = {
+				humanoid: {
+					pointer: {
+						look: ["Pointer", "Lookpad"],
+					},
+					key: {
+						open_menu: ["KeyQ", "Backquote"],
+					},
+				},
+				menu: {
+					key: {
+						close_menu: ["KeyQ", "Backquote"],
+					},
+				},
+			} satisfies Bindings1
+			
+			const version = detect_bindings_version(test_bindings)
+			expect(version).equals(1)
+		},
+
+		"can detect version from modern bindings (v2 and up)": async () => {
+			const test_bindings = {
+				version: 2,
+				modes: {
+					humanoid: {
+						pointer: {
+							look: [["Pointer"], ["Lookpad"]],
+						},
+						key: {
+							open_menu: [["KeyQ"], ["Backquote"]],
+						},
+					},
+					menu: {
+						key: {
+							close_menu: [["KeyQ"], ["Backquote"]],
+						},
+					},
+				}
+			} satisfies Bindings2
+
+			const version = detect_bindings_version(test_bindings)
+			expect(version).equals(2)
+		},
+
+	},
+	"bindings converters": {
+
+		"bindings 1 to 2": {
+
+			"can convert empty bindings1 to bindings2": async () => {
+				const b1: Bindings1 = {}
+				const b2 = converter_1(b1)
+				const modes_array = Object.entries(b2.modes)
+
+				expect(modes_array.length).equals(0)
+			},
+
+			"can convert ordinary bindings1 to bindings2": async () => {
+				const test_bindings = {
+					humanoid: {
+						pointer: {
+							look: ["Pointer", "Lookpad"],
+						},
+						key: {
+							open_menu: ["KeyQ", "Backquote"],
+						},
+					},
+					menu: {
+						key: {
+							close_menu: ["KeyQ", "Backquote"],
+						},
+					},
+				} satisfies Bindings1
+
+				const b2 = converter_1(test_bindings)
+				expect(b2.version).equals(2)
+			},
+
 		},
 
 	},
